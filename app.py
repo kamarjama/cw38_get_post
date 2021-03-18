12425fa53f0da70e97609026fca1bc99b2ce8ea1
HSA-Student <student.k.jama29@horizoncolumbus.org>
2021-03-18 11:41:55 -0400

    finished cw38

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        name = request.form['username']
+        return render_template("index.html", content="Hello" + name)
+    else:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
